03d5659eb38e5aac7a0e488a56928282871f824b
ayesha-siddiqa20 <ayeshaahmeds222@gmail.com>
2023-04-03 02:30:25 -0400

    changed again settings.py to fix path to static

--- server/p2/p2/settings.py
+++ server/p2/p2/settings.py
@@ -15,7 +15,6 @@ from datetime import timedelta
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -59,7 +58,7 @@ ROOT_URLCONF = 'p2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'client/p3/build')],
+        'DIRS': [os.path.join(BASE_DIR, '../../client/p3/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -145,4 +144,4 @@ AUTH_USER_MODEL = 'accounts.User'
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60)
 }
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'client/p3/build/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '../../client/p3/build/static')]
