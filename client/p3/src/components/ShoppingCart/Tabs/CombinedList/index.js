bb3805fc3a44ff2da0570b8386892afa7d01fe4e
faisalt14 <faisalt14@gmail.com>
2023-04-10 14:47:33 -0400

    added api calls to server to obtain combined shopping list, added api call to clear cart as well

--- client/p3/src/components/ShoppingCart/Tabs/CombinedList/index.js
+++ client/p3/src/components/ShoppingCart/Tabs/CombinedList/index.js
@@ -1,9 +1,61 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./style.css"
 
 const CombinedList = () => {
+    const [combinedData, setCombinedData] = useState([]);
+    const [token, setToken] = useState("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjgxMTU1ODcxLCJpYXQiOjE2ODExNTIyNzEsImp0aSI6Ijc2OTJiZjc0MmRjODQzM2VhY2Y4YTA3N2NlOTM4NmIyIiwidXNlcl9pZCI6MX0.JyOlGnXti0piNT2trfCu03Ib23VReMl92BWWjKMAHeA")
+    const [clearCartClicks, setclearCartClicks] = useState(0); 
+
+    const [value, setValue] = useState(['inital'])
+
+    const clearCart = () => {
+        setclearCartClicks(clearCartClicks + 1)
+
+        fetch(" http://127.0.0.1:8000/accounts/shopping-list/clear/", {
+            method: 'DELETE',
+            headers: { 
+                'Content-Type': 'application/json',
+               'Authorization': `Bearer ${token}`,
+              }
+            
+        })
+       
+
+
+
+    }
+
+
+
+    useEffect( () => {
+        
+
+        {/** Reference: https://reqbin.com/code/javascript/ricgaie0/javascript-fetch-bearer-token */}
+        fetch("http://127.0.0.1:8000/accounts/combined-list/", {
+            headers: { 
+                'Content-Type': 'application/json',
+               'Authorization': `Bearer ${token}`,
+              }
+            
+        })
+            .then(response => response.json())
+            .then(json => {
+                setCombinedData(json); 
+                
+            })
+
+
+    }, [clearCartClicks])
+
+    // console.log(combinedData)
+
+
+
+    
     return (
         <>
+
+                
             <table>
                 <thead>
                     <tr>
@@ -23,30 +75,21 @@ const CombinedList = () => {
                     <tbody className="body">
 
                         <div className="ingredient-quantity-container"> 
-                            <tr>
-                                <td className="bodyIngredient"> Ingrediet 1</td>
-                                <td className="bodyQuantity"> Quantity 1</td>
-                            
-                            </tr>
-
-                            <tr>
-                            <td className="bodyIngredient"> Ingrediet 2</td>
-                            <td className="bodyQuantity"> Quantity 2</td>
-                        
-                            </tr>
-
-                            <tr>
-                            <td className="bodyIngredient"> Ingrediet 3</td>
-                            <td className="bodyQuantity"> Quantity 3</td>
-                        
-                            </tr>
+                            { 
+                                combinedData.map( (dict, index) => (
+                                    <tr key={index}>
+                                        <td className="bodyIngredient"> {dict.name} </td>
+                                        <td className="bodyQuantity"> {dict.quantity} {dict.unit}</td>
+                                    </tr>
+                                ))
+                            }
                         </div>              
                     </tbody>
                 </div>     
         </table>
 
         <div className="clearButtonContainer">
-            <button id="clearButton"> Empty Shopping Cart</button>
+            <button id="clearButton" onClick={() => clearCart()}> Empty Shopping Cart</button>
         </div>
         
         
--- client/p3/src/components/ShoppingCart/index.js
+++ client/p3/src/components/ShoppingCart/index.js
@@ -39,6 +39,8 @@ const ShoppingCart = () => {
                     {activeTab === "tab1" ? <CombinedList/> : <IndividualList/>}
                 </div>
             </div>
+
+
         
         </>
     )
