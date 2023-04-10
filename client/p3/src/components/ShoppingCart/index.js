00e2136469110819a9f3b33576652c30353ac74e
faisalt14 <faisalt14@gmail.com>
2023-04-09 21:28:21 -0400

    Added ShoppingCart, Tabs, IndividualList, and CombinedList starter components

---
+++ client/p3/src/components/ShoppingCart/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './style.css'; 
+
+const ShoppingCart = () => {
+    return (
+        <>
+            <h1>Shopping Cart Page</h1>
+        </>
+    )
+
+}
+
+export default ShoppingCart;
